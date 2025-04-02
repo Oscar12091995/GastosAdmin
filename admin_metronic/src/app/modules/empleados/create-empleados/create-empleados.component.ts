0051fd340b4c651f13e26abc40478e59bfe5898b
Oscar12091995 <quike0116@hotmail.com>
2025-04-01 22:17:11 -0600

    Se preparo la visualizcion de imagenes y las variables para insercion de empleados

--- admin_metronic/src/app/modules/empleados/create-empleados/create-empleados.component.ts
+++ admin_metronic/src/app/modules/empleados/create-empleados/create-empleados.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+import { EmpleadosService } from '../service/empleados.service';
 
 @Component({
   selector: 'app-create-empleados',
@@ -6,5 +9,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-empleados.component.scss']
 })
 export class CreateEmpleadosComponent {
+is_genero:any = 'masculino';
+tab_selected:number = 1;
+nombre:string = '';
+avatar:any;
+avatar_previsualiza:any = 'assets/media/svg/files/blank-image.svg';
+appaterno:string = '';
+apmaterno:string = '';
+curp:string = '';
+telefono:string = '';
+genero:string = '';
+rfc:string = '';
+seguro_social:string = '';
+departamento_id:number = 1;
+puesto_id:number = 1;
+fecha_ingreso:Date;
+domicilio:string = '';
+colonia:string = '';
+municipio:string = '';
+isLoading:any;
+
+constructor(
+  public toast: ToastrService,
+  public empleadoService: EmpleadosService
+){
+
+}
+
+ngOnInit(): void{
+  this.isLoading = this.empleadoService.isLoading$;
+}
+
+isloadingProccess(){
+  this.empleadoService.isLoadingSubject.next(true);
+  setTimeout(() => {
+    this.empleadoService.isLoadingSubject.next(false);
+  }, 50);
+}
+
+processFile($event:any){
+  if ($event.target.files[0].type.indexOf("image") < 0) {
+    this.toast.warning("WARN", "El archivo no es una imagen");
+    return false;
+  }
+  this.avatar = $event.target.files[0];
+  let reader = new FileReader();
+  reader.readAsDataURL(this.avatar);
+  reader.onloadend = () => this.avatar_previsualiza = reader.result;
+  this.isloadingProccess();
+}
+
+selectedGenero(val:any){
+  this.is_genero = val;
+}
+
+selectedTab(val:number){
+this.tab_selected = val;
+}
 
 }
--- admin_metronic/src/app/modules/empleados/service/empleados.service.ts
+++ admin_metronic/src/app/modules/empleados/service/empleados.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { AuthService } from '../../auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmpleadosService {
 
-  constructor() { }
+   isLoading$: Observable<boolean>;
+      isLoadingSubject: BehaviorSubject<boolean>;
+
+    constructor(
+      //petciones http al backend
+          private http: HttpClient,
+          //servicio de logeo para el prefijo del api auth
+          public authservice: AuthService,
+    ) {
+      this.isLoadingSubject = new BehaviorSubject<boolean>(false);
+      this.isLoading$ = this.isLoadingSubject.asObservable();
+    }
 }
